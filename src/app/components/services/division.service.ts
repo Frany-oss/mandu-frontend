8856f0ca1935d19748452c237f99c5ba4058fe29
Frany-oss <francesco8bassino@gmail.com>
2025-07-19 19:20:55 -0500

    agregando el link del backend desplegado

--- src/app/components/services/division.service.ts
+++ src/app/components/services/division.service.ts
@@ -2,25 +2,26 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { CreateDivision, DvisionViewModel } from '../entities/divisions.entity';
 import { firstValueFrom } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DivisionService {
   private http = inject(HttpClient);
+  private readonly apiUrl = environment.apiUrl;
 
   constructor() {}
 
-  // get all divisions
   async getAllDivisions() {
     return await firstValueFrom(
-      this.http.get<DvisionViewModel[]>('http://localhost:3000/divisions')
+      this.http.get<DvisionViewModel[]>(`${this.apiUrl}/divisions`)
     );
   }
 
   async createDivision(division: CreateDivision) {
     return await firstValueFrom(
-      this.http.post<DvisionViewModel>('http://localhost:3000/divisions', division)
+      this.http.post<DvisionViewModel>(`${this.apiUrl}/divisions`, division)
     );
   }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  apiUrl: 'https://mandu-backend.onrender.com'
+};
